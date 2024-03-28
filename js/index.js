c526196add9677729555b4db88940d8cada8a634
tema <35255039+spadre@users.noreply.github.com>
2024-03-28 08:55:23 +0200

    Added new board button that asks user x and y row amount, also button deletes old board

--- js/index.js
+++ js/index.js
@@ -1,18 +1,54 @@
 function createStartingDivs(){
-    const containerDiv = document.querySelector(".container");
     
-    for(i=0;i < 16; i++){
+    const newBoardBtn = document.querySelector("#newBoard");
+    
+    newBoardBtn.addEventListener("click",() =>{
+        createNewBoard();
+    });
+
+    createBoard(16,16);
+    hoverMouse();
+
+}
+
+// function asks user to input x and y rows (max 100) and creates new board, also deletes old board
+
+function createNewBoard(){
+let xDivCount = prompt("Creating new board\n How many rows for x aligment (max 100)"), yDivCount = prompt("Creating new board\n How many rows for y aligment (max 100)");
+
+document.querySelectorAll(".xDiv").forEach((divX) => {
+    divX.remove();
+});
+
+createBoard(xDivCount,yDivCount);
+
+
+}
+
+
+// function creates new board based on xDiv and yDiv inputs
+function createBoard(xDiv,yDiv){
+
+    if(xDiv > 100){xDiv = 100}
+    if (yDiv > 100){yDiv = 100} 
+
+    const containerDiv = document.querySelector(".container");
+    for(i=0;i < xDiv; i++){
         let xDiv = document.createElement("div");
         xDiv.setAttribute("class","xDiv");
         containerDiv.appendChild(xDiv);
-        for(j=0;j<16;j++){
+        for(j=0;j<yDiv;j++){
             let yDiv = document.createElement("div");
             yDiv.setAttribute("class","yDiv");
             xDiv.appendChild(yDiv);
-            //let yDiv = document.querySelector(".yDiv");
             
         }
     }
+    hoverMouse();
+}
+
+// hovering mouse over divs changes div to red and leaving div leaves color blue
+function hoverMouse(){
     const divs = document.querySelectorAll(".yDiv");
     divs.forEach((divY) => {
         divY.addEventListener("mouseover",() => {
@@ -23,8 +59,6 @@ function createStartingDivs(){
         });
     });
 }
-
-function createNewBoard(){}
 
 
 createStartingDivs();
